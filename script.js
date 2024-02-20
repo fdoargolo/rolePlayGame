86c9848aed304d513e0047a38c101c9427fd095a
fdoargolo <felipeargolo75@gmail.com>
2024-02-20 18:07:01 -0300

    creating and updating some functions and also creating the kil monster object

--- script.js
+++ script.js
@@ -74,7 +74,13 @@ const locations = [
     "button text": ["Attack","Dodge","Run"],
     "button functions": [attack,dodge,goTown],
     text: "You are fighting a monster."
-  } 
+  },
+  {
+    name: "kill monster",
+    "button text": ["Go to town square","Go to town square","Go to town square"],
+    "button functions": [goTown,goTown,goTown],
+    text: "The monster screams Arg! as it dies. You gain experience points and find gold."
+  }
 ];
 
 //initialize buttons.
@@ -151,26 +157,57 @@ function goCave(){
     }
   }
 
-  function fightSlime(){
-
-  }x
-
-  function fightBeast(){
-
+  function fightSlime() {
+    fighting = 0;
+    goFight();
   }
-
-  function fightDragon(){
-    console.log("Fighting dragon.");;
+  
+  function fightBeast() {
+    fighting = 1;
+    goFight();
+  }
+  
+  function fightDragon() {
+    fighting = 2;
+    goFight();
   }
 
   function goFight() {
-
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = "block";
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsterHealth;
   }
   
   function attack() {
+    text.innerText = "The " + monsters[fighting].name + " attacks.";
+    text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
+    health -= monsters[fighting].level;
+    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+    healthText.innerText = health;
+    monsterHealthText.innerText = monsterHealth;
+  
+    if (health <= 0) {
+      lose();
+    } else if(monsterHealth <= 0){
+      defeatMonster();
+    }
   
   }
   
   function dodge() {
+    text.innerText = "You dodge the attack from the "+ monsters[fighting].name;
+  }
   
+  function defeatMonster() {
+    gold += Math.floor(monsters[fighting].level * 6.7);
+    xp += monsters[fighting].level;
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+    update(locations[4]);
+  }
+
+  function lose(){
+    
   }
