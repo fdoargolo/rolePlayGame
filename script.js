43496c3d68a86ab6781af0eb219f5aa268ba1768
fdoargolo <felipeargolo75@gmail.com>
2024-02-21 23:27:17 -0300

    creating and updating player actions

--- script.js
+++ script.js
@@ -86,6 +86,12 @@ const locations = [
    "button text": ["REPLAY?","REPLAY?","REPLAY?"],
    "button functions": [restart,restart,restart],
    text: "You die. &#x2620;"
+  },
+  {
+    name: "win",
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+    "button functions": [restart, restart, restart],
+    text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;"
   }
 ];
 
@@ -190,23 +196,35 @@ function goCave(){
   function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks.";
     text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
-    health -= monsters[fighting].level;
-    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+    health -= getMonsterAttackValue(monsters[fighting].level);
+    if (isMonsterHit()) {
+      monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;    
+    } else {
+      text.innerText += " You miss.";
+    }
     healthText.innerText = health;
     monsterHealthText.innerText = monsterHealth;
-  
     if (health <= 0) {
       lose();
-    } else if(monsterHealth <= 0){
-      if(fighting === 2){
+    } else if (monsterHealth <= 0) {
+      if (fighting === 2) {
         winGame();
-      }else{
-      defeatMonster();
+      } else {
+        defeatMonster();
+      }
     }
-    }
-  
+  }
+
+  function getMonsterAttackValue(level) {
+    const hit = (level * 5) - (Math.floor(Math.random() * xp));
+    console.log(hit);
+    return hit > 0 ? hit : 0;
   }
   
+  function isMonsterHit() {
+    return Math.random() > .2 || health < 20;
+  }
+
   function dodge() {
     text.innerText = "You dodge the attack from the "+ monsters[fighting].name;
   }
